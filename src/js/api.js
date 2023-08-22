b8519682a7f61ee47ac9e3dbf26ee92c0eeac817
Potapenko Anna <potapka23@gmail.com>
2023-08-22 23:36:04 +0300

    completed

--- src/js/api.js
+++ src/js/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_KEY = '38951602-28f9062c7efd02f2b96d1092a';
-const URL = 'https://pixabay.com/api/';  
+const BASE_URL = 'https://pixabay.com/api/';
 
 async function fetchImages(query, page, perPage) {
   const params = new URLSearchParams({
@@ -14,8 +14,8 @@ async function fetchImages(query, page, perPage) {
     query: query,
   });
 
-  const response = await axios.get(`${URL}?key=${API_KEY}&${params}`);
+  const response = await axios.get(`${BASE_URL}?key=${API_KEY}&${params}`);
   return response.data;
-}
+} 
 
 export { fetchImages };
